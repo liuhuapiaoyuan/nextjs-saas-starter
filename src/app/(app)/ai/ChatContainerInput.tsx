10c0e98c0baf98dc74e74afd38a0dfa46d597627
liuhuapiaoyuan <278780765@qq.com>
2024-09-20 11:38:01 +0800

    fix:  textarea auto adjust height

--- src/app/(app)/ai/ChatContainerInput.tsx
+++ src/app/(app)/ai/ChatContainerInput.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { MicrochipIcon } from 'lucide-react'
+import { Mic } from 'lucide-react'
 import { submit } from './actions'
 import { useFormStatus } from 'react-dom'
+import { Textarea } from '@/components/ui/textarea'
 
 function SubmitButton() {
   const { pending } = useFormStatus()
@@ -20,30 +21,26 @@ export function ChatContainerInput() {
       action={async formData => {
         const content = formData.get('chat-input')?.toString()
         content && (await submit(content))
+        return ''
       }}
     >
       <label htmlFor='chat-input' className='sr-only'>
         Enter your prompt
       </label>
-      <div className='relative'>
-        <Button
-          variant='ghost'
-          className='absolute group hover:bg-transparent top-0 bottom-0 px-2 h-auto '
-        >
-          <MicrochipIcon className='text-muted-foreground group-hover:text-foreground' />
+      <div className='relative w-full  items-end resize-none rounded-xl border-none bg-slate-200 p-2  text-sm text-slate-900 focus:outline-none focus:ring-2 focus:ring-primary dark:bg-slate-800 dark:text-slate-200 dark:placeholder-slate-400 dark:focus:ring-blue-500 sm:text-base flex'>
+        <Button variant='ghost' className=' group hover:bg-transparent  px-2  '>
+          <Mic className='text-muted-foreground group-hover:text-foreground' />
         </Button>
-        <textarea
+        <Textarea
           id='chat-input'
           name='chat-input'
-          className='block w-full resize-none rounded-xl border-none bg-slate-200 p-4 pl-10 pr-20 text-sm text-slate-900 focus:outline-none focus:ring-2 focus:ring-primary dark:bg-slate-800 dark:text-slate-200 dark:placeholder-slate-400 dark:focus:ring-blue-500 sm:text-base'
+          className='block flex-1 w-1 hov border-0 focus-visible:ring-0 focus-visible:shadow-none'
           placeholder='Enter your prompt'
           rows={1}
           required
         />
 
-        <div className='absolute group hover:bg-transparent top-0 bottom-0 right-0 px-2 h-auto  flex items-center'>
-          <SubmitButton />
-        </div>
+        <SubmitButton />
       </div>
     </form>
   )
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,24 +1,42 @@
-import * as React from "react"
+'use client'
+import * as React from 'react'
 
-import { cn } from "@/lib/utils"
-
+import { cn } from '@/lib/utils'
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
+    const textAreaRef = React.useRef<HTMLTextAreaElement>(null)
+    React.useImperativeHandle(ref, () => textAreaRef.current!)
+    React.useEffect(() => {
+      if (textAreaRef.current) {
+        textAreaRef.current.style.height = 'auto'
+      }
+    }, [props.value])
+    const handleInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      if (textAreaRef.current) {
+        textAreaRef.current.style.height = 'auto'
+      }
+      const scrollHeight = event.target.scrollHeight // 获取内容高度
+      event.target.style.height = `${scrollHeight}px` // 设置内容高度
+
+      props.onInput?.(event)
+    }
+
     return (
       <textarea
+        ref={textAreaRef}
         className={cn(
-          "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          'flex w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
-        ref={ref}
+        onInput={handleInput} // 添加事件处理
         {...props}
       />
     )
   }
 )
-Textarea.displayName = "Textarea"
+Textarea.displayName = 'Textarea'
 
 export { Textarea }
